d790372a25facd0b354e974ca06c678ea064e228
vpsingh16 <144575378+vpsingh16@users.noreply.github.com>
2024-08-13 21:19:34 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -86,7 +86,7 @@ async function displayAlbums() {
         if (e.href.includes("/songs") && !e.href.includes(".htaccess")) {
             let folder = e.href.split("/").slice(-2)[0]
             // Get the metadata of the folder
-            let a = await fetch(`/songs/${folder}/info.json`)
+            let a = await fetch(`./songs/${folder}/info.json`)
             let response = await a.json(); 
             cardContainer.innerHTML = cardContainer.innerHTML + ` <div data-folder="${folder}" class="card">
             <div class="play">
@@ -97,7 +97,7 @@ async function displayAlbums() {
                 </svg>
             </div>
 
-            <img src="/songs/${folder}/cover.jpg" alt="">
+            <img src="./songs/${folder}/cover.jpg" alt="">
             <h2>${response.title}</h2>
             <p>${response.description}</p>
         </div>`
@@ -108,7 +108,7 @@ async function displayAlbums() {
     Array.from(document.getElementsByClassName("card")).forEach(e => { 
         e.addEventListener("click", async item => {
             console.log("Fetching Songs")
-            songs = await getSongs(`songs/${item.currentTarget.dataset.folder}`)  
+            songs = await getSongs(`./songs/${item.currentTarget.dataset.folder}`)  
             playMusic(songs[0])
 
         })
@@ -117,7 +117,7 @@ async function displayAlbums() {
 
 async function main() {
     // Get the list of all the songs
-    await getSongs("songs")
+    await getSongs("./songs")
     playMusic(songs[0], true)
 
     // Display all the albums on the page
@@ -203,11 +203,6 @@ async function main() {
         }
 
     })
-
-
-
-
-
 }
 
 main() 
